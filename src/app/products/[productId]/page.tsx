0d5b3fe6a0c26283737aa143d42fa34d1fff8b63
balajiyatheysh <balajiyatheysh87@gmail.com>
2023-11-21 16:30:13 +0530

    page.tsx title modified

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,6 +1,26 @@
-export default function ProductDetails ({params}:{
-  params:{productId:string}
-}){
+import { Metadata } from "next"
+
+type Props ={
+  params:{
+    productId: string
+  }
+}
+
+
+export const generateMetadata = async ({params}:Props):Promise<Metadata>=>{
+  const {productId}=params;
+  const title = await new Promise((resolve)=>{
+    setTimeout(() => {
+      resolve(productId)
+    }, 100);
+  });
+  return {
+    title: `Product ${title}`
+  }
+}
+
+
+export default function ProductDetails ({params}:Props){
   return (
     <div>
        <h1>Product Details about {params.productId}</h1> 
