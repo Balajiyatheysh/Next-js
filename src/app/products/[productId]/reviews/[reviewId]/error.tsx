ca54d0061c3cee56ade3d69e0fd9b786cb470d88
balajiyatheysh <balajiyatheysh87@gmail.com>
2024-01-06 22:46:29 +0530

    recovering from errors

--- src/app/products/[productId]/reviews/[reviewId]/error.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/error.tsx
@@ -1,4 +1,14 @@
-"use client"
-export default function ErrorBoundry ({error}: {error : Error}){
-  return <h4>{error.message}</h4>
+"use client";
+export default function ErrorBoundry({
+  error,
+  reset,
+}: {
+  error: Error;
+  reset: () => void;
+}) {
+  return (
+    <h4>
+      {error.message} <button onClick={reset}>Try again</button>
+    </h4>
+  );
 }
