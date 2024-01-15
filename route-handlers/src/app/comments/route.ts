00dce9a236c043d21d888425483de00f529a415d
balajiyatheysh <balajiyatheysh87@gmail.com>
2024-01-15 22:34:14 +0530

    http methods

--- route-handlers/src/app/comments/route.ts
+++ route-handlers/src/app/comments/route.ts
@@ -1,20 +1,30 @@
+import { NextRequest } from "next/server";
 import { comments } from "./data";
 
-export async function GET (){
-  return  Response.json(comments);
+export async function GET(request : NextRequest) {
+
+  const queryParams = request.nextUrl.searchParams;
+
+  const query=queryParams.get("query");
+
+  const filteredComments = query ? comments.filter(comment => comment.text.includes(query)) : comments;
+
+  return Response.json(filteredComments);
+  
 }
 
-export async function POST (request : Request){
+export async function POST(request: Request) {
   const comment = await request.json();
   const newComment = {
-    id: comments.length +1,
-    text: comment.text
-  }
-  comments.push(newComment)
-  return new Response(JSON.stringify(newComment),{
-    headers:{
-      "content-type":"application/json"
+    id: comments.length + 1,
+    text: comment.text,
+  };
+  comments.push(newComment);
+  return new Response(JSON.stringify(newComment), {
+    headers: {
+      "content-type": "application/json",
     },
-    status:201
-  })
+    status: 201,
+  });
 }
+
